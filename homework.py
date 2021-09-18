6a2b35184e2af35c55ddc8406f729332301e9a7d
Смирнов Олег Владимирович <osmirnov.rabota@gmail.com>
2021-09-18 17:27:49 +0300

    Second commit. Fixed 1) Add extra blank strings between docstrings and the main code, 2) Delite unnecessary methods from CashCalculator and CaloriesCalculator, 3) in Record class fixed date condition 4) Create new method stat_for_today in Calculate class for calculate daily limit, 5)Use new method in CashCalc and CaloriesCalc as condition, 6) in get_today_cash_remained use Guard Block to rewrite condition. create new var(money_format) to storage currensy from dict. also add new condition for checking currency. so if user uses wrong currency the code wont crash, 7) also fixed other remarks.

--- homework.py
+++ homework.py
@@ -8,15 +8,13 @@ class Record:
     amount (int), comment (default = 'без комментария')
     and date (datetime.date).
     If user didn't mention the date - the class takes present day."""
+
     def __init__(self, amount, comment='без комментария', date=None):
+        self.amount = amount
+        self.comment = comment
         if date is None:
-            self.amount = amount
-            self.comment = comment
-            now = dt.datetime.now()
-            self.date = now.date()
+            self.date = dt.date.today()
         else:
-            self.amount = amount
-            self.comment = comment
             user_date = dt.datetime.strptime(date, '%d.%m.%Y')
             self.date = user_date.date()
 
@@ -27,24 +25,35 @@ class Calculator:
     The class can creates list of users, calculates
     daily statistics (money expenses or calories gained)
     and calculates statistics for last 7 days."""
+
     def __init__(self, limit):
         self.limit = limit
         self.records = []
 
     def add_record(self, record):
         """"The method "add_record adds" user's records in list."""
+
         self.records.append(record)
 
     def get_today_stats(self):
         """"The method "get_today_stats" can calculate statistics for present date by
         compare dates in records to present dates."""
+
         now = dt.date.today()
         return sum(record.amount for record in self.records
                    if record.date == now)
 
+    def stat_for_today(self):
+        """"The method returns the different between
+        limit and expenses for present day and so
+        you know you limit."""
+
+        return self.limit - self.get_today_stats()
+
     def get_week_stats(self):
         """"The method "get_week_stats" can calculate statistics for last 7 days by
         compare dates in records to 7 days ago date and present date."""
+
         now = dt.date.today()
         week_ago = now - dt.timedelta(days=7)
         return sum(record.amount for record in self.records
@@ -55,24 +64,21 @@ class CaloriesCalculator(Calculator):
     """"Class name: CaloriesCalculator. Description:
     the class gives information about
     calories have been eaten for present day and for last 7 days."""
+
     def get_calories_remained(self):
-        today_stats = super().get_today_stats()
-        balance = self.limit - today_stats
+        balance = self.stat_for_today()
         if balance > 0 and balance < self.limit:
             return (f'Сегодня можно съесть что-нибудь ещё, '
                     f'но с общей калорийностью не более {balance} кКал')
         else:
             return 'Хватит есть!'
 
-    def get_week_stats(self):
-        balanc_for_7_days = super().get_week_stats()
-        return f'Поглащено за последние 7 дней {balanc_for_7_days}: кКал.'
-
 
 class CashCalculator(Calculator):
     """"Class name: CashCalculator. Description: Description:
     the class gives information about
     money spent that have been done for present day and for last 7 days."""
+
     USD_RATE = 72.55
     EURO_RATE = 85.37
     RUB_RATE = 1.0
@@ -87,22 +93,22 @@ class CashCalculator(Calculator):
         for present day in chosen currency.
         If eur or usd was chosen the method
         converts rubles to these currencies."""
+
         self.currency = currency
-        today_stats = super().get_today_stats()
-        balance = round((self.limit - today_stats)
-                        / CashCalculator.currencies[currency][1], 2)
+        if currency not in self.currencies:
+            format = (', '.join(list(self.currencies)))
+            return (f'Введеннй формато валют не поддерживается.'
+                    f'Введите {format}')
+        balance = self.stat_for_today()
+        if balance == 0:
+            return 'Денег нет, держись'
+        balance = round((balance)
+                        / self.currencies[currency][1], 2)
+        money_format = self.currencies[currency][0]
         if balance > 0:
             return (f'На сегодня осталось '
-                    f'{balance} {CashCalculator.currencies[currency][0]}')
-        elif balance == 0:
-            return 'Денег нет, держись'
+                    f'{balance} {money_format}')
         else:
             balance = abs(balance)
             return (f'Денег нет, держись: твой долг - '
-                    f'{balance} {CashCalculator.currencies[currency][0]}')
-
-    def get_week_stats(self):
-        """The method get_week_stats just shows
-        how much money was spend by user in last 7 days."""
-        balanc_for_7_days = super().get_week_stats()
-        return f'Потрачено за последние 7 дней: {balanc_for_7_days} рублей.'
+                    f'{balance} {money_format}')
